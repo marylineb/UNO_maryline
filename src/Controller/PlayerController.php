d53aedf589a291ffaad257443d4df120f56f20fe
Maryline <philippemarylineb@gmail.com>
2026-03-22 09:36:45 +0100

    style fini

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -35,18 +35,55 @@ final class PlayerController extends AbstractController
             return $this->redirectToRoute('play');
         }
 
-        // Si le joueur n'a aucune carte jouable, il pioche automatiquement
-        if (!$player->hasPlayableCard($topCard)) {
-            $drawnCard = $gameState->drawCard();
+        $drawStack = $gameState->getDrawStack();
 
-            if ($drawnCard instanceof Card) {
-                $player->addCard($drawnCard);
+        /*
+         * CAS 1 : le joueur subit un empilement de +2
+         * Il ne peut répondre qu'avec un +2.
+         * Sinon il pioche tout le stack puis passe son tour.
+         */
+        if ($drawStack > 0) {
+            $hasPlusTwo = false;
+
+            foreach ($player->getHand() as $handCard) {
+                if ($handCard->getValue() === '+2') {
+                    $hasPlusTwo = true;
+                    break;
+                }
             }
 
-            $gameState->nextPlayer();
-            $session->set('game', $gameState->toArray());
+            if (!$hasPlusTwo) {
+                for ($i = 0; $i < $drawStack; $i++) {
+                    $drawnCard = $gameState->drawCard();
 
-            return $this->redirectToRoute('play');
+                    if ($drawnCard instanceof Card) {
+                        $player->addCard($drawnCard);
+                    }
+                }
+
+                $gameState->resetDrawStack();
+                $gameState->nextPlayer();
+                $session->set('game', $gameState->toArray());
+
+                return $this->redirectToRoute('play');
+            }
+        } else {
+            /*
+             * CAS 2 : pas de +2 empilé
+             * Si aucune carte jouable, le joueur pioche 1 puis passe.
+             */
+            if (!$player->hasPlayableCard($topCard)) {
+                $drawnCard = $gameState->drawCard();
+
+                if ($drawnCard instanceof Card) {
+                    $player->addCard($drawnCard);
+                }
+
+                $gameState->nextPlayer();
+                $session->set('game', $gameState->toArray());
+
+                return $this->redirectToRoute('play');
+            }
         }
 
         $cardIndex = filter_var($request->query->get('id'), FILTER_VALIDATE_INT);
@@ -61,43 +98,54 @@ final class PlayerController extends AbstractController
             return $this->redirectToRoute('play');
         }
 
-        $canPlay = $card->getColor() === $topCard->getColor()
-            || $card->getValue() === $topCard->getValue();
+        /*
+         * Si drawStack > 0, seule une carte +2 est autorisée.
+         * Sinon, règle normale : même couleur ou même valeur.
+         */
+        if ($drawStack > 0) {
+            if ($card->getValue() !== '+2') {
+                return $this->redirectToRoute('play');
+            }
+        } else {
+            $canPlay = $card->getColor() === $topCard->getColor()
+                || $card->getValue() === $topCard->getValue();
 
-        if (!$canPlay) {
-            return $this->redirectToRoute('play');
+            if (!$canPlay) {
+                return $this->redirectToRoute('play');
+            }
         }
 
         $playedCard = $player->removeCardByIndex($cardIndex);
 
-        if ($playedCard) {
-            $gameState->addToDiscardPile($playedCard);
-
-            switch ($playedCard->getValue()) {
-                case 'X':
-                    $gameState->setSkipNextPlayer(true);
-                    break;
-
-                case 'S':
-                    $gameState->reverseDirection();
-                    break;
-
-                case '+2':
-                    $gameState->addDrawStack(2);
-                    break;
-            }
-
-            if ($player->hasWon()) {
-                $gameState->setGameOver(true);
-                $gameState->setWinnerId($player->getId());
-                $session->set('game', $gameState->toArray());
-
-                return $this->redirectToRoute('end');
-            }
-
-            $gameState->nextPlayer();
+        if (!$playedCard) {
+            return $this->redirectToRoute('play');
         }
 
+        $gameState->addToDiscardPile($playedCard);
+
+        switch ($playedCard->getValue()) {
+            case 'X':
+                $gameState->setSkipNextPlayer(true);
+                break;
+
+            case 'S':
+                $gameState->reverseDirection();
+                break;
+
+            case '+2':
+                $gameState->addDrawStack(2);
+                break;
+        }
+
+        if ($player->hasWon()) {
+            $gameState->setGameOver(true);
+            $gameState->setWinnerId($player->getId());
+            $session->set('game', $gameState->toArray());
+
+            return $this->redirectToRoute('end');
+        }
+
+        $gameState->nextPlayer();
         $session->set('game', $gameState->toArray());
 
         return $this->redirectToRoute('play');
